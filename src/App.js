b1351874fdc31366bcb8aea639a5e3368df78dcf
Podr <umais.mateen@gmail.com>
2021-01-17 01:43:17 +0500

    Lazy Loading...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter,Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+
+
+const asyncCheckout = asyncComponent(()=>{
+  return import('./containers/Checkout/Checkout')
+});
+const asyncOrders = asyncComponent(()=>{
+  return import('./containers/Orders/Orders')
+});
+const asyncAuth = asyncComponent(()=>{
+  return import('./containers/Auth/Auth')
+});
 
 
 class App extends Component {
@@ -22,9 +32,9 @@ class App extends Component {
       <Layout>
         <div>
           <Switch>
-            <Route path="/auth" component={Auth} />
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
+            {!this.props.isAuthenticated ? <Route path="/auth" component={asyncAuth} /> : null}
+            <Route path="/checkout" component={asyncCheckout} />
+           {this.props.isAuthenticated ? <Route path="/orders" component={asyncOrders} /> :null }
             <Route path="/logout" component={Logout} />
             <Route path="/" component={BurgerBuilder} />
             <Redirect to="/" />
